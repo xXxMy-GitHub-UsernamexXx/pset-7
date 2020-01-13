440c9b44e27312154a2e86e58f7c64f089f3226c
Ethan Charles <ECharles1@ucvts.tec.nj.us>
2020-01-13 13:11:37 -0500

    continued endsMeet function

--- src/exercises.js
+++ src/exercises.js
@@ -18,18 +18,20 @@ function commonEnd(a, b) {
 function endsMeet(values, n) {
         const newValues = [];
 
-        if (!values || values.length >= n || !n || n >= 0 || isNaN(Number(n))) {
+        if (!values || (values.length - 1 <= n) || !n || n >= 0 || isNaN(Number(n))) {
                 return newValues;
         }
 
         for (var i = 0; i < n; i++) {
-                values[i] = newValues[i];
+                let placeholderA;
+                values[i] = placeholderA;
+                newValues.push(placeholderA);
         }
 
         for (var j = n; j > 0; j--) {
-                let placeholder;
-                values[values.length - j] = placeholder;
-                newValues.push(placeholder);
+                let placeholderB;
+                values[(values.length  - 1) - j] = placeholderB;
+                newValues.push(placeholderB);
         }
 }
 
